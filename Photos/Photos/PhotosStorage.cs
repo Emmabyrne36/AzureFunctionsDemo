f62a207f5fbb0e8004d36af96d1b8f4bf002c13d
emmabyrne36 <emmabyrne36@gmail.com>
2022-06-21 14:16:03 +0100

    Refactor to use latest package for uploading blob

--- Photos/Photos/PhotosStorage.cs
+++ Photos/Photos/PhotosStorage.cs
@@ -1,25 +1,53 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Microsoft.WindowsAzure.Storage.Blob;
+using Photos.Models;
 
 namespace Photos
 {
     public static class PhotosStorage
     {
+        private const string BlobContainerName = "photos";
+
         [FunctionName("PhotosStorage")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
-            [Blob("photos", FileAccess.ReadWrite, Connection = Literals.StorageConnectionString)] CloudBlobContainer blobContainer,
             ILogger logger)
         {
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            var request = JsonConvert.DeserializeObject<PhotoUploadModel>(body);
 
+            var newId = Guid.NewGuid();
+            var blobName = $"{newId}.jpg";
+
+            var blob = GetBlobClient(blobName);
+            var photoBytes = Convert.FromBase64String(request.Photo);
+
+            using (var stream = new MemoryStream(photoBytes))
+            {
+                await blob.UploadAsync(stream);
+            }
+
+            logger?.LogInformation($"Successfully uploaded {blobName} file");
+
+            return new OkObjectResult(newId);
+        }
+
+        private static BlobClient GetBlobClient(string blobName)
+        {
+            var connectionString = Environment.GetEnvironmentVariable(Literals.StorageConnectionString);
+            var blobClient = new BlobContainerClient(connectionString, BlobContainerName);
+
+            var blob = blobClient.GetBlobClient(blobName);
+
+            return blob;
         }
     }
 }
